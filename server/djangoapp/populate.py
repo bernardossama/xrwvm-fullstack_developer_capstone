ab161f9ff8a32b07baf71d18f0fb8a419d9cd9c7
bernardossama <bernard.ragheb@gmail.com>
2024-04-16 01:17:12 +0200

    testing populate.py for lint

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -109,8 +109,9 @@ def initiate():
       },
     ]
 
-    for data in car_model_data: CarModel.objects.create(
-        name=data['name'],
-        car_make=data['car_make'],
-        type=data['type'],
-        year=data['year'])
+    for data in car_model_data: 
+          CarModel.objects.create( # type: ignore
+               name=data['name'],
+               car_make=data['car_make'],
+               type=data['type'],
+               year=data['year'])
