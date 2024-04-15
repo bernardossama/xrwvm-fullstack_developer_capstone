d2c336aeb6998184b2143de7a9ae57530e8c93ab
bernardossama <bernard.ragheb@gmail.com>
2024-04-16 01:38:12 +0200

    testing populate.py for lint 5

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -109,7 +109,9 @@ def initiate():
       },
     ]
 
-    loop = [CarModel.objects.create(name=data['name'],
-                                    car_make=data['car_make'],
-                                    type=data['type'],
-                                    year=data['year']) for data in car_model_data]
+    loop = [CarModel.objects.create(
+        name=data['name'],
+        car_make=data['car_make'],
+        type=data['type'],
+        year=data['year']) for data in car_model_data]
+    print(loop)
