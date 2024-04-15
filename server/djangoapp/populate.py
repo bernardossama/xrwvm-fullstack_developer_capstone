f31e22406410aa2bd20158319b789cebdc728fdd
bernardossama <bernard.ragheb@gmail.com>
2024-04-16 00:10:32 +0200

    models.py lint adjustments done

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,8 +12,9 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-       car_make_instances.append(CarMake.objects.create(name=data['name'],
-                                                        description=data['description']))
+       car_make_instances.append(CarMake.objects.create(
+          name=data['name'],
+          description=data['description']))
 
     car_model_data = [
       {
@@ -53,20 +54,20 @@ def initiate():
            "car_make": car_make_instances[1]
       },
       {
-           "name":"A4",
-           "type":"SUV",
+           "name": "A4",
+           "type": "SUV",
            "year": 2023,
-           "car_make":car_make_instances[2]
+           "car_make": car_make_instances[2]
       },
       {
-           "name":"A5",
-           "type":"SUV",
+           "name": "A5",
+           "type": "SUV",
            "year": 2023,
-           "car_make":car_make_instances[2]
+           "car_make": car_make_instances[2]
       },
       {
-           "name":"A6",
-           "type":"SUV",
+           "name": "A6",
+           "type": "SUV",
            "year": 2023,
            "car_make": car_make_instances[2]
       },
