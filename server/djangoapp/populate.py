01ca36911810205808dd1c243f56e6d5fb3722d7
bernardossama <bernard.ragheb@gmail.com>
2024-04-16 01:09:17 +0200

    testing populate.py for lint

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -12,9 +12,9 @@ def initiate():
 
     car_make_instances = []
     for data in car_make_data:
-       car_make_instances.append(CarMake.objects.create(
-          name=data['name'],
-          description=data['description']))
+        car_make_instances.append(CarMake.objects.create(
+             name=data['name'],
+             description=data['description']))
 
     car_model_data = [
       {
@@ -110,8 +110,8 @@ def initiate():
     ]
 
     for data in car_model_data:
-       CarModel.objects.create(
-         name=data['name'],
-         car_make=data['car_make'],
-         type=data['type'],
-         year=data['year'])
+          CarModel.objects.create(
+              name=data['name'],
+              car_make=data['car_make'],
+              type=data['type'],
+              year=data['year'])
